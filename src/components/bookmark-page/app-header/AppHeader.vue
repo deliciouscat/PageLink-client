b2d3468df3029612da6b8495cc6bde3723c5fbfd
deliciouscat <sunmo009@gmail.com>
2025-09-16 17:08:02 +0900

    app-header 슈도코드 작성, chrome extention manifest 작성

---
+++ src/components/bookmark-page/app-header/AppHeader.vue
@@ -0,0 +1,29 @@
+<!--
+# Pseudo Code
+
+## 임포트
+import chrome-api
+import app-header, bottom-sheet, collections
+
+## 데이터 클래스(collection 및 bookmarks 정보, 메모, 환경설정 값 등)
+## 컴포지트 패턴
+DataClass (
+    
+)
+
+## 컴포넌트
+VerticalGrid(
+    app-header,
+    bottom-sheet,
+    collections
+)
+
+## app-header의 `검색`/`collection 추가` 컴포넌트를 사용해 `collections` 조작
+
+
+
+
+
+
+
+-->

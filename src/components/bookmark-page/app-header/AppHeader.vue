83238240b6843f19ed7607af57793e3f65165572
deliciouscat <sunmo009@gmail.com>
2025-09-21 16:34:02 +0900

    DataComponents 슈도코드 작성

--- src/components/bookmark-page/app-header/AppHeader.vue
+++ src/components/bookmark-page/app-header/AppHeader.vue
@@ -5,13 +5,7 @@
 import chrome-api
 import app-header, bottom-sheet, collections
 
-## 데이터 클래스(collection 및 bookmarks 정보, 메모, 환경설정 값 등)
-## 컴포지트 패턴
-DataClass (
-    
-)
-
-## 컴포넌트
+## UI 컴포넌트
 VerticalGrid(
     app-header,
     bottom-sheet,

da1b2c952e0d0fad71a445bd884b932a52b42c73
deliciouscat <sunmo009@gmail.com>
2025-10-23 18:24:40 +0900

    로그인 화면  슈도코드. with Clerk

---
+++ src/components/auth-settings-page/MyPage.vue
@@ -0,0 +1,16 @@
+// 대충 Placeholder
+<template>
+  <div class="my-page">
+    <h1>My Page</h1>
+    <h2>User Information 들어갈 구석임</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.my-page {
+  display: flex;
+  flex-direction: column;
+}
